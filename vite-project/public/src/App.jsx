bfcdb650fbc5c1ae2e06e787ee31adbd1427fb94
sa7ya <131731688+Sa7yaC@users.noreply.github.com>
2025-02-23 21:33:20 +0530

    ui chnages

---
+++ vite-project/public/src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from "./components/ui/button"
+function App() {
+
+
+  return (
+    <>
+    <div className="px-20"></div>
+     <Button>Hello World</Button>
+    </>
+  )
+}
+
+export default App
